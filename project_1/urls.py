45b134e46bc2186a6402ced74f7f0f5fe2a7be76
Tesfahun Berhanu <tesfahunt@gmail.com>
2022-01-23 18:10:16 -0800

    adding superuser as an admin

--- project_1/urls.py
+++ project_1/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from app_1 import views as app_1_views
+admin.site.site_header = 'Yonas LLC Admin'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', app_1_views.index, name='index'),
     path('about/', app_1_views.about, name='about'),
     path('contact/', app_1_views.contact, name='contact'),
